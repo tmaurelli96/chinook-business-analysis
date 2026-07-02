5cc2e6b367750512920b46fbc119b2aedbd99d8d
tmaurelli96 <tiziano.maurelli@yahoo.it>
2026-07-02 10:07:48 +0200

    Create 02_top_customers.sql

---
+++ sql/02_top_customers.sql
@@ -0,0 +1,13 @@
+SELECT
+    c.FirstName,
+    c.LastName,
+    SUM(i.Total) AS Total_Spent
+FROM customers c
+INNER JOIN invoices i
+    ON c.CustomerId = i.CustomerId
+GROUP BY
+    c.CustomerId,
+    c.FirstName,
+    c.LastName
+ORDER BY Total_Spent DESC
+LIMIT 10;
