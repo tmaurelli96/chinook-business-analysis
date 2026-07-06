30788fdec22f06a928c823883d4f477591d77bce
tmaurelli96 <tiziano.maurelli@yahoo.it>
2026-07-06 13:36:50 +0200

    Update 06_invoices_above_average.sql

--- sql-advanced/06_invoices_above_average.sql
+++ sql-advanced/06_invoices_above_average.sql
@@ -1,3 +1,8 @@
+🇬🇧 Business Question
+Which invoices have a total amount higher than the average invoice value?
+🇮🇹 Domanda di Business
+Quali fatture hanno un importo superiore al valore medio delle fatture?
+
 SELECT InvoiceID, Total
 FROM invoices i
 WHERE i.Total > (
